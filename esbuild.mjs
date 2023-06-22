c409b8779d5af30970256f9ba084f777a7717bd2
oostach <oleksandr.ostach@gmail.com>
2023-06-23 02:19:15 +0300

    Start watching if env is development

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,30 +3,40 @@ import { sassPlugin } from "esbuild-sass-plugin";
 import postcss from 'postcss';
 import autoprefixer from 'autoprefixer';
 
-// Generate CSS/JS Builds
-let ctx = await esbuild.context({
-      entryPoints: [
-        { out: 'application', in: "app/assets/stylesheets/application.scss" },
-        { out: 'application', in: "app/javascript/application.js" },
-      ],
-      outdir: "app/assets/builds",
-      bundle: true,
-      metafile: true,
-      target: 'es2020',
-      sourcemap: true,
-      minify: false,
-      loader: { '.ts': 'ts' },
-      publicPath: '/assets',
-      plugins: [
-          sassPlugin({
-              async transform(source) {
-                  const { css } = await postcss([autoprefixer]).process(source, { from: undefined });
-                  return css;
-              },
-          }),
-      ],
-  });
+let config = {
+  entryPoints: [
+    { out: 'application', in: "app/assets/stylesheets/application.scss" },
+    { out: 'application', in: "app/javascript/application.js" },
+  ],
+  outdir: "app/assets/builds",
+  bundle: true,
+  metafile: true,
+  target: 'es2020',
+  sourcemap: true,
+  minify: process.env.RAILS_ENV != "development",
+  loader: {
+    '.ts': 'ts',
+    '.tsx': 'tsx'
+  },
+  publicPath: '/assets',
+  plugins: [
+    sassPlugin({
+      async transform(source) {
+        const { css } = await postcss([autoprefixer]).process(source, { from: undefined });
+        return css;
+      },
+    }),
+  ],
+};
 
-await ctx.watch()
-  .then(() => console.log("⚡ Build complete! ⚡"))
-  .catch(() => process.exit(1));
+// Generate CSS/JS Builds
+if (process.env.RAILS_ENV == "development") {
+  let ctx = await esbuild.context(config);
+  await ctx.watch()
+    .then(() => console.log("⚡ Build complete! ⚡"))
+    .catch(() => process.exit(1));
+} else {
+  await esbuild.build(config)
+    .then(() => console.log("⚡ Build complete! ⚡"))
+    .catch(() => process.exit(1));
+}
