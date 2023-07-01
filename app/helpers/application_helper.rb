8d01ead0fbdf6253a0780d741195993540ad5418
oostach <oleksandr.ostach@gmail.com>
2023-07-01 03:32:44 +0300

    Add helper method to render flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  def flash_message
+    capture do
+      flash.each do |type, message|
+        concat content_tag(:div, message, class: ['flash', type])
+      end
+    end
+  end
 end
