5ee413ffbec0b20a52da03998d7c18db1afc37d6
oostach <oleksandr.ostach@gmail.com>
2023-07-02 23:02:16 +0300

    Add "main menu" and "user bar"

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  def index; end
-
-  def show; end
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root 'dashboard#index'
+  root 'dashboard#show'
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
   post '/signup', to: 'users#create'
   get '/signup', to: 'users#new'
+
+  resource :profile
 end
