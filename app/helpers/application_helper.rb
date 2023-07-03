a95f6e69ee8e5615fcdcb9aabf3faba17ca4a29c
oostach <oleksandr.ostach@gmail.com>
2023-07-03 23:43:20 +0300

    Add close button to the flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
+
 module ApplicationHelper
   def flash_message
     capture do
       flash.each do |type, message|
-        concat content_tag(:div, message, class: ['flash', type])
+        concat(
+          content_tag(:div, class: ['flash', type]) do
+            content_tag(:span, message) + link_to(svg_icon('x-mark'))
+          end
+        )
       end
     end
   end
---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module LayoutHelper
+  class SvgNotFound < StandardError; end
+
+  def svg_icon(name, path: nil, classes: '', options: {})
+    svg_file = find_svg_file(name, path)
+
+    body   = svg_file.to_s.force_encoding('UTF-8')
+    doc    = Nokogiri::HTML::DocumentFragment.parse body
+    svg    = doc.at_css 'svg'
+
+    update_icon_attributes(svg, classes:, options:)
+    raw doc # rubocop:disable Rails/OutputSafety
+  end
+
+  def update_icon_attributes(svg, classes:, options:)
+    svg['class'] = classes
+    options.each { |k, v| svg[k] = v }
+  end
+
+  def find_svg_file(name, path)
+    svg_path      = path || 'icons/svg'
+    svg_file_path = File.join(svg_path, "#{name}.svg")
+    svg_file      = AssetsDetector.find_assets_source(svg_file_path)
+
+    raise SvgNotFound, "Can't find #{svg_file_path}" unless svg_file
+
+    svg_file
+  end
+end
