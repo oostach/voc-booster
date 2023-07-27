40cad2ed023891bf01b3146042986cc3aeaebac8
oostach <oleksandr.ostach@gmail.com>
2023-07-27 12:09:08 +0300

    Do not create user for confirmation email preview

--- test/mailers/previews/registration_mailer_preview.rb
+++ test/mailers/previews/registration_mailer_preview.rb
@@ -3,7 +3,7 @@
 # Preview all emails at http://voc.dev/rails/mailers/registration_mailer
 class RegistrationMailerPreview < ActionMailer::Preview
   def confirmation_email
-    user = FactoryBot.create(:user)
+    user = User.new(email: 'test@voc-booster.com', first_name: 'Jane', last_name: 'Doe', confirmation_token: SecureRandom.hex)
 
     RegistrationMailer.with(user: user).confirmation_email
   end
